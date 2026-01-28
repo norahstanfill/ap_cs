f21b46edfde7d7ea3b230da69cf68aa40934f67f
norah.stanfill <norah.stanfill@students.atlantaclassical.org>
2026-01-28 14:34:55 -0500

    Norah is the best

---
+++ test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void print_0_10()
+{
+    for(int i=0; i<=10; i++)
+    { 
+        printf("%d\n",i);
+
+    
+    
+    }
+}
+
+void print_2_20(){
+
+    for(int i=2; i<=20; i++)
+    {
+        printf("%d\n", i);
+    }
+
+}
+
+void print_4_40()
+    for(int i=4; i<=40; i++)
+    { 
+        printf("%d\n", i);
+    }
+
+int main(){
+print_0_10();
+print_2_20();
+print_4_40();
+
+}
