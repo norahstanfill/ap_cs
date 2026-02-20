e1eaf9c01349d1a43edc0b60971bae10e220f67d
norah.stanfill <norah.stanfill@students.atlantaclassical.org>
2026-02-20 14:17:33 -0500

    im norah

--- feb13.py
+++ feb13.py
@@ -1,19 +1,32 @@
-print("hello world")
-age = 25 
-print(25)
-var = "first_norah"
-var last = "last_stanfill"
-var x = 10
-var y = 5 
-print(10+5)
-print(10-5)
-print(10*5)
-print(10/5)
-temp = 72
-(72>65)
-var a = 8
-var b = 12
-whether(8<12, 12=12)
+var city = ("london")
+print("city")
+x = 12
+y= 5
+print(x*y)
+print(x-y)
+print(x/y)
+print(x//y)
+base = 10
+height = 5
+print area(0.5*base*height)
+print(10 - 2*3)
+print((10-2)*3)
+is_open = false
+if("is_open")
+print(false)
+p = 15 
+q = 20
+bool (p > q)
+print(p !=q)
+temp = 75
+if (temp > 80)
+print("is_sunny")
+
+
+
+
+
+
 
 
 
